2699bba4b26373add2b918d41949fa16dcaf6494
Luca Stefani <luca020400@lineageos.org>
2017-08-23 16:40:39 +0200

    oneplus3: Add seccomp policy
    Change-Id: I9a7e49ee0c78c782afc3791bad2ecfc8ee7d2bb5

--- device.mk
+++ device.mk
@@ -347,6 +347,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
