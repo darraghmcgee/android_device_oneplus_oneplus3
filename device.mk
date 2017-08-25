6a97d810dc7f921e9efe76a8a5d865e5b99eaf49
Luca Stefani <luca020400@lineageos.org>
2017-08-25 22:16:01 +0200

    oneplus3: Add DRM hal
    Change-Id: Ib3edf76e9df72c347351c1484a9ceee66f1a3dc0

--- device.mk
+++ device.mk
@@ -184,6 +184,10 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+
 # Doze mode
 PRODUCT_PACKAGES += \
     OneplusDoze
