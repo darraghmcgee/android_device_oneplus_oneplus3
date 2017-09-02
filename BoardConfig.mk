fe05e58316ebc0907de17d08098d29fa1400ac5a
Darragh McGee <darraghmcgee20@gmail.com>
2017-09-02 23:50:25 +0200

    oneplus3: Disable qcom sepolicies
    Change-Id: I2b6d4413b05c868f99cf196a7a4f63b8d3e95849

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -213,8 +213,7 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_op3
 TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-
+#include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Sensors
